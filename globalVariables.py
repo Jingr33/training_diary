858dc1792f689f610f080985b49b841a3f5a4d19
JiriIngr <jirka.ingr@seznam.cz>
2024-03-16 14:08:33 +0100

    Nastavení -> Přepínač auto-fullscreen dokončen

--- globalVariables.py
+++ globalVariables.py
@@ -1,9 +1,25 @@
-"""Tady se ukládají globální porměnné."""
-# tréninky v databázi nejsou indexované, ale při každém spuštění souboru se každému tréninku jeden index přiřadí, 
-#aby se s nimi dalo dle potřeby nakládat
-# index tréninku
-training_id = 0
-# metoda pro zvyšování id 
-def increaseID (id : int) -> int:
-    """Metoda zvýší id tréninku o 1."""
-    return id + 1
+"""NAČTENÍ NASTAVENÍ Z DATABÁZE UŽIVATELSKÉHO NASTAVENÍ APLIKACE"""
+def loadSettingDatabase () -> dict:
+    """Načte databázi nastavení aplikace do slovníku."""
+    with open ("setting_database.txt", "r") as f:
+        lines = f.readlines()
+        global setting
+    setting = {}
+    for line in lines:
+        key, value = line.split(":")
+        setting[key] = value
+    return setting
+
+def overwriteSettingFile () -> None:
+    """Přepíše databázi nastavení aplikace, uloží nově provedené změny."""
+    file_lines = [None] * len(setting)
+    i = 0
+    for item in setting:
+        one_line = item + ":" + str(setting[item])
+        file_lines[i] = one_line
+        i = i + 1
+    with open ("setting_database.txt", "w") as f:
+        f.writelines(file_lines)
+
+def getAutoFullscreen () -> int:
+    return int(setting["auto-fullscreen"])
--- settingOption/settingFrame.py
+++ settingOption/settingFrame.py
@@ -1,10 +1,12 @@
 # import knihoven
 from tkinter import *
 import customtkinter as ctk
+from icecream import ic
 # import souborů
 from settingOption.sportSelectionFrame import SportSelectionFrame
 from ctkWidgets import Frame, Label, Entry, Switch, Button
 from configuration import colors
+import globalVariables as GV
 
 class SettingFrame (Frame):
     """Načte frame s widgetami pro manuální nastavení různých funkcí aplikace."""
@@ -12,7 +14,7 @@ class SettingFrame (Frame):
         super().__init__(master)
         self.master = master
         self.side_pad = 15
-        self.columnconfigure([0, 1], weight = 1)
+        self.columnconfigure([0, 1], weight = 0)
         self.rowconfigure([0,1,2,3,4,6,7], weight = 1)
         self._initGUI()
 
@@ -35,9 +37,11 @@ class SettingFrame (Frame):
         """Vytvoří widgety s nastavení zapínání okna na fullscreen při spuštění."""
         option_name = Label(self, "Celá obrazovka", self.name_font)
         option_name.grid(row = 1, column = 0, sticky = ctk.W, padx = self.side_pad)
+        ic(GV.setting["auto-fullscreen"])
         self.switch_value = IntVar()
-        switcher = Switch(self, "", ..., self.switch_value)
-        switcher.grid(row = 1, column = 1, sticky = ctk.E, padx = (0, self.side_pad))
+        self.switch_value.set(GV.getAutoFullscreen())
+        switcher = Switch(self, "", self._setAutoFullscreen, self.switch_value)
+        switcher.grid(row = 1, column = 1, padx = (0, self.side_pad))
         description = Label(self, "Při spuštění zobrazí aplikaci na celou obrazovku.")
         description.grid(row = 2, column = 0, columnspan = 2, sticky = ctk.W, pady = self.bottom_pad, padx = self.side_pad)
 
@@ -46,7 +50,7 @@ class SettingFrame (Frame):
         option_name = Label(self, "Tréninkové plány", self.name_font)
         option_name.grid(row = 3, column = 0, sticky = ctk.W, padx = self.side_pad)
         plan_button = Button(self, "Přehled", ...)
-        plan_button.grid(row = 3, column = 1, sticky = ctk.E, padx = (0, self.side_pad))
+        plan_button.grid(row = 3, column = 1, padx = (0, self.side_pad))
         plan_button.configure(width = 130)
         description = Label(self, "Zobrazí přehled tréninkových plánu. Umožňuje jejich mazání.")
         description.grid(row = 4, column = 0, sticky = ctk.W, columnspan = 2, padx = self.side_pad, pady = self.bottom_pad)
@@ -57,7 +61,7 @@ class SettingFrame (Frame):
         option_name.grid(row = 5, column = 0, sticky = "NW", padx = self.side_pad)
         option_name.configure(height = 35)
         sport_selection = SportSelectionFrame(self)
-        sport_selection.grid(row = 5, column = 1, rowspan = 2, sticky = ctk.E, padx = (0, self.side_pad), pady = self.bottom_pad)
+        sport_selection.grid(row = 5, column = 1, rowspan = 2, padx = (0, self.side_pad), pady = self.bottom_pad)
         description = Label(self, "Přidání či odebrání odtrénovaných sportů a nastavení jejich barvy v aplikaci.")
         description.grid(row = 6, column = 0, sticky = "NW", padx = self.side_pad, pady = self.bottom_pad)
 
@@ -67,7 +71,12 @@ class SettingFrame (Frame):
         option_name.grid(row = 7, column = 0, sticky = ctk.W, padx = self.side_pad)
         self.var_rows = StringVar()
         rows_entry = Entry(self, self.var_rows)
-        rows_entry.grid(row = 7, column = 1, sticky = ctk.E, padx = (0, self.side_pad))
+        rows_entry.grid(row = 7, column = 1, padx = (0, self.side_pad))
         rows_entry.configure(width = 130)
         description = Label(self, "Počet řádků zobrazovaných na straánce přehledu tréninků.")
         description.grid(row = 8, column = 0, columnspan = 2, sticky = ctk.W, padx = self.side_pad, pady = self.bottom_pad)
+
+    def _setAutoFullscreen (self) -> None:
+        """Uloží nově nastavenou hodnotu nastavení velikosti okna při zapnutí aplikace."""
+        GV.setting["auto-fullscreen"] = self.switch_value.get()
+        GV.overwriteSettingFile()
