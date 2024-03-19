e5478d058e173b7458ace6b900ab9913ae79b41c
JiriIngr <jirka.ingr@seznam.cz>
2024-03-19 21:26:15 +0100

    Zapisování zvolených sportů do souboru -> zabugované, nefunguje správně zápis prvního sportu.

--- globalVariables.py
+++ globalVariables.py
@@ -12,11 +12,14 @@ def loadSettingDatabase () -> dict:
         setting[key] = value
     global sport_colors
     sport_colors = getSportColors()
+    global selected_sports
+    selected_sports = getSelectedSports()
     return setting
 
 def overwriteSettingFile () -> None:
     """Přepíše databázi nastavení aplikace, uloží nově provedené změny."""
     setting["sport-colors"] = prepareSportColorsToFile()
+    setting["selected-sports"] = prepareSelectedSportsToFile()
     file_lines = [None] * len(setting)
     i = 0
     for item in setting:
@@ -40,13 +43,26 @@ def getSportColors () -> dict:
     sport_colors["běh"] = sport_colors["beh"]
     del sport_colors["beh"]
     return sport_colors
+
+def getSelectedSports () -> list:
+    """Vrátí list hodnot 1 / 0, které udávájí, zda se daný sport mám zorbazovat v aplikaci nebo ne."""
+    selected_list = setting["selected-sports"].split(";") # rozdělení na hodnoty
+    del selected_list[-1] # vymazání poslední přebývající hodnoty
+    for i in range(len(selected_list)): # převedení na int
+        selected_list[i] = int(selected_list[i])
+    return selected_list
     
 def prepareSportColorsToFile () -> str:
     """Vytvoří ze slovníku řádek pro zapsání do souboru. Vrátí string."""
-    ic(sport_colors)
     string = ""
     for key in sport_colors:
-        ic(key)
         string = "{0}{1}={2};".format(string, key, sport_colors[key])
     string = string.replace("běh", "beh")
+    return string
+
+def prepareSelectedSportsToFile () -> str:
+    """Vytvoří string z listu selected_sports pro zapsání zpět do souboru., Vrátí string."""
+    string = ""
+    for item in selected_sports:
+        string = "{0}{1};".format(string, str(item))
     return string
--- settingOption/sportSelectionFrame.py
+++ settingOption/sportSelectionFrame.py
@@ -26,17 +26,21 @@ class SportSelectionFrame (Frame):
         buttons = [None] * len(sport_list)
         vars = [None] * len(sport_list)
         for i in range(len(sport_list)):
+            #label
             sport_name = Label(self, sport_list[i], ("Arial", 14, "normal"))
             sport_name.grid(row = i, column = 0, sticky = ctk.W, padx = padx_label, pady = pady)
+            #checkbox
             cb_var = IntVar()
+            cb_var.set(GV.selected_sports[i])
             select_cb = CheckBox(self, "", cb_var)
             select_cb.grid(row = i, column = 1, padx = padx_cb, pady = pady)
-            select_cb.configure(width = 27)
+            select_cb.configure(width = 27, fg_color = GV.sport_colors[sport_list[i]], command = lambda: self._setSelectedSports(i))
             checkboxes[i] = select_cb
             vars[i] = cb_var
+            # button
             choose_color = Button(self, "Změnit barvu", lambda: self._initColorPicker(i))
             choose_color.grid(row = i, column = 2, pady = pady)
-            choose_color.configure(width = 100, height = 27)
+            choose_color.configure(width = 100, height = 27, fg_color = GV.sport_colors[sport_list[i]])
             buttons[i] = choose_color
         # slovník potřebných widgetů
         self.sport_widgets = {
@@ -51,7 +55,21 @@ class SportSelectionFrame (Frame):
         color = pick_color.get()
         if color:
             self.sport_widgets["button"][index].configure(fg_color = color)
-            self.sport_widgets["checkbox"] [index].configure(fg_color = color)
+            self.sport_widgets["checkbox"][index].configure(fg_color = color)
             GV.sport_colors[sport_list[index]] = color
-            ic(GV.sport_colors)
             GV.overwriteSettingFile()
+
+    def _setSelectedSports(self, index : int) -> None:
+        """Nastaví změny ve vybraných sportech, uloží nastavení do databáze."""
+        changed_value = int(self.sport_widgets["var"][index].get())
+        ic(changed_value)
+        GV.selected_sports[index] = changed_value
+        self._changeStateOfButton(changed_value, index)
+        GV.overwriteSettingFile()
+
+    def _changeStateOfButton (self, changed_value : int, index : int) -> None:
+        """Změní nastavení stavu tlačítka pro výběr barvy na disabled v případě, že sport není vybrán."""
+        if changed_value:
+            self.sport_widgets["button"][index].configure(state = "normal")
+        else:
+            self.sport_widgets["button"][index].configure(state = "disabled")
